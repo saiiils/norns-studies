b0d5d13d550230dd71cba5fcb795b3c01c336141
saiiils <matthew.kim64@gmail.com>
2022-12-11 13:41:44 -0800

    finished study 2

--- study2.lua
+++ study2.lua
@@ -1,46 +1,81 @@
 -- study 2
 -- code exercise
--- conditional reveal
+-- procedural: indexing
+
+engine.name = "PolyPerc"
 
 function init()
-  level = 3
-  number = 84
-  mode = 0
+  engine.release(3)
+  notes = {}        -- notes table
+  selected = {}     -- selected table to track playing notes
+  
+  -- create 5x5 square of notes:
+  for m = 1,5 do
+    notes[m] = {}
+    selected[m] = {}
+    for n = 1,5 do
+      -- n is horizontal index
+      notes[m][n] = 55 * 2 ^ ((m * 12 + n * 2) / 12) -- fancy math to get some notes
+      selected[m][n] = false -- all start unselected
+    end
+  end
+  light = 0
+  number = 3 -- max number of notes to play at once
 end
 
 function redraw()
   screen.clear()
-  if mode == 0 then
-    screen.level(level)
-    screen.font_face(10)
-    screen.font_size(20)
-    screen.move(0,50)
-    screen.text("number: " .. number)
-    screen.update()
-  elseif mode == 1 then
-    screen.move(0,20)
-    screen.text("WILD")
-    screen.aa(1)
-    screen.line_width(2)
-    screen.move(60,30)
-    screen.line(80,40)
-    screen.line(90,10)
-    screen.close()
-    screen.stroke()
-    screen.update()
+  for m = 1,5 do
+    for n = 1,5 do
+      screen.rect(0.5 + m * 9, 0.5 + n * 9, 6, 6) -- (x, y, width, height)
+      l = 2
+      if selected[m][n] then
+        l = l + 3 + light
+      end
+      screen.level(l)
+      screen.stroke()
+    end
   end
+  screen.move(10,60)
+  screen.text(number)
+  screen.update()
 end
 
 function key(n,z)
-  if n == 3 then
-    mode = z
-  else
-    level = 3 + z * 12
+  if n == 2 and z == 1 then
+    -- clear selected notes
+    for x = 1,5 do
+      for y = 1,5 do
+        selected[x][y] = false
+      end
+    end
+    -- choose new random notes
+    for i = 1,number do
+      selected[math.random(5)][math.random(5)] = true
+    end
+  elseif n == 3 then
+    -- find notes to play
+    if z == 1 then -- k3 pressed
+      for x=1,5 do
+        for y=1,5 do
+          if selected[x][y] then
+            engine.hz(notes[x][y])
+          end
+        end
+      end
+      light = 7 -- adds 7 to the square's screen leevl
+    elseif z == 0 then -- k3 not pressed
+      light = 0 -- adds - to the square's screen level
+    end
   end
   redraw()
 end
 
 function enc(n,d)
-  number = number + d
+  if n == 3 then
+    -- clamp number of notes from 1 to 4
+    number = util.clamp(number + d, 1, 4)
+  end
   redraw()
 end
+      
